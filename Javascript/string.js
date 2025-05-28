30269898a14a4ba5358c7e5bbeb61c30bf82b2fb
Himesh Ganorkar <ganorkarhimesh67@gmail.com>
2025-05-28 08:31:18 +0530

    Add files via upload

---
+++ Javascript/string.js
@@ -0,0 +1,6 @@
+function inputFunction() {
+            var number = document.getElementById('input').value  
+        document.getElementById('display').innerHTML = number ;
+        alert(number);
+        console.log(number);
+    }
